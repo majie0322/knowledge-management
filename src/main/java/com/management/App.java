5e249e806885cd7b3171f777270fa85762a829ea
nicky_chen <cx52018761@hotmail.com>
2017-06-02 16:00:33 +0800

    initial

--- src/main/java/com/management/App.java
+++ src/main/java/com/management/App.java
@@ -1,13 +1,9 @@
 package com.management;
 
-import com.management.conf.CORSConfiguration;
-import com.management.conf.MyConfiguration;
 import com.management.interceptor.Interceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 /**
