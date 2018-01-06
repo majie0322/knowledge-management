f10bf21954f1207049bffc5133bc5be2bd490713
majie0322 <15715849816@163.com>
2018-01-06 22:36:45 +0800

    redis

--- src/test/java/com/huizhi/management/dao/BdUserDaoTest.java
+++ src/test/java/com/huizhi/management/dao/BdUserDaoTest.java
@@ -2,18 +2,23 @@ package com.huizhi.management.dao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import com.huizhi.management.util.RedisUtilsTemplate;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class BdUserDaoTest {
 
+    @Autowired
+    private RedisUtilsTemplate redisUtilsTemplate;
+
     @Test
     public void findUserByName() throws Exception {
-
+        //redisUtilsTemplate.set("a", "111");
+        System.err.println(redisUtilsTemplate.get("a"));
     }
 
     @Test
